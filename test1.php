69d4774ea715ba434116d9fbddad3237f809ebb7
taku8a <89015721+taku8a@users.noreply.github.com>
2023-12-05 20:35:06 +0900

    [New]新規作成
    test1.phpを新規作成

---
+++ test1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test1</title>
+</head>
+<body>
+  <?php
+    echo 'Hello World!';
+  ?>  
+</body>
+</html>
